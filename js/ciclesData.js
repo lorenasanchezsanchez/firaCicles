7b4de89b5adc8fa1922c0f7dd914a694ef3a5f1c
Lorena Sánchez Sánchez <l.sanchezsanchez2@edu.gva.es>
2026-04-30 19:09:28 +0200

    Update ciclesData.js

--- js/ciclesData.js
+++ js/ciclesData.js
@@ -86,7 +86,49 @@ export const ciclesData = {
     },
     daw: {
       titol: "Desenvolupament d'Aplicacions Web",
-      descripcio: "DAW"
+      descripcio: "DAW",
+        pla: [
+  '<div class="panel-section">',
+  '<h3>📚 Pla de formació de 2000 hores</h3>',
+  '<div class="plan-formacio">',
+
+  // 1r Curs
+  '<div class="curs">',
+    '<h4>1r curs</h4>',
+    '<ul>',
+      '<li>Bases de dades</li>',
+      '<li>Entorns de desenvolupament</li>',
+      '<li>Formació i orientació laboral</li>',
+      '<li>Llenguatges de marques i sistemes de gestió de la informació</li>',
+      '<li>Programació</li>',
+      '<li>Sistemes informàtics</li>',
+      '<li>Anglès tècnic (GS)</li>',
+      '<li>Itinerari personal per a l\'ocupabilitat I</li>',
+      '<li>Projecte intermodular de desenvolupament d\'aplicacions web (I)</li>',
+    '</ul>',
+  '</div>',
+
+  // 2n Curs
+  '<div class="curs">',
+    '<h4>2n curs</h4>',
+    '<ul>',
+      '<li>Desenvolupament web en entorn client</li>',
+      '<li>Desenvolupament web en entorn servidor</li>',
+      '<li>Desplegament d\'aplicacions web</li>',
+      '<li>Disseny d\'interfície web</li>',
+      '<li>Empresa i iniciativa emprenedora</li>',
+      '<li>Itinerari personal per a l\'ocupabilitat II</li>',
+      '<li>Projecte intermodular de desenvolupament d\'aplicacions web (II)</li>',
+      '<li>Mòdul optatiu</li>',
+      '<li>Digitalització aplicada al sistema productiu (GS)</li>',
+      '<li>Sostenibilitat aplicada al sistema productiu</li>',
+      '<li>Formació en centres de treball (FCT)</li>',
+    '</ul>',
+  '</div>',
+
+  '</div>',
+  '</div>'
+]
     },
     fpb: {
       titol: "Formació Professional Bàsica",
