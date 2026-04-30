5b0fd5d2b6fe004968249fb92e672e63e8f1c2b6
Lorena Sánchez Sánchez <l.sanchezsanchez2@edu.gva.es>
2026-04-30 18:36:26 +0200

    Create ciclesData.js

---
+++ js/ciclesData.js
@@ -0,0 +1,103 @@
+ciclesData = {
+    smx: {
+      titol: "Sistemes Microinformàtics i Xarxes",
+      descripcio: "SMX",
+      pla: [
+      '<div class="panel-section">',
+      '<h3>📚 Pla de formació de 2000 hores</h3>',
+
+      '<div class="plan-formacio">',
+
+      '<div class="curs">',
+      '<h4>1r curs</h4>',
+      '<ul>',
+      '<li>Montatge i manteniment d’equips</li>',
+      '<li>Sistemes operatius monopost</li>',
+      '<li>Aplicacions ofimàtiques</li>',
+      '<li>Xarxes locals</li>',
+      '<li>Anglés professional (GM)</li>',
+      '<li>Itinerari personal per a l’ocupabilitat I</li>',
+      '</ul>',
+      '</div>',
+
+      '<div class="curs">',
+      '<h4>2n curs</h4>',
+      '<ul>',
+      '<li>Sistemes operatius en xarxa</li>',
+      '<li>Seguretat informàtica</li>',
+      '<li>Serveis en xarxa</li>',
+      '<li>Aplicacions web</li>',
+      '<li>Mòdul optatiu</li>',
+      '<li>Itinerari personal per a l’ocupabilitat II</li>',
+      '<li>Sostenibilitat aplicada al sistema productiu</li>',
+      '<li>Digitalització aplicada al sistema productiu</li>',
+      '<li>Projecte intermodular</li>',
+      '<li>Formació en Centres de Treball (FCT)</li>',
+      '</ul>',
+      '</div>',
+
+      '</div>',
+      '</div>'
+    ]
+    },
+    dam: {
+      titol: "Desenvolupament d'Aplicacions Multiplataforma",
+      descripcio: "DAM",
+      pla: [
+          '<div class="panel-section">',
+          '<h3>📚 Pla de formació de 2000 hores</h3>',
+        
+          '<div class="plan-formacio">',
+        
+          '<div class="curs">',
+          '<h4>1r curs</h4>',
+          '<ul>',
+          '<li>Anglés professional (GS)</li>',
+          '<li>Llenguatges de marques i sistemes de gestió d\'informació</li>',
+          '<li>Sistemes informàtics</li>',
+          '<li>Bases de dades</li>',
+          '<li>Programació</li>',
+          '<li>Entorns de desenvolupament</li>',
+          '<li>Itinerari personal per a l\'ocupabilitat I</li>',
+          '<li>Projecte intermodular de desenvolupament d\'aplicacions multiplataforma (primer curs)</li>',
+          '</ul>',
+          '</div>',
+        
+          '<div class="curs">',
+          '<h4>2n curs</h4>',
+          '<ul>',
+          '<li>Accés a dades</li>',
+          '<li>Desenvolupament d\'interfícies</li>',
+          '<li>Programació multimèdia i dispositius mòbils</li>',
+          '<li>Programació de serveis i processos</li>',
+          '<li>Sistemes de gestió empresarial</li>',
+          '<li>Itinerari personal per a l\'ocupabilitat II</li>',
+          '<li>Projecte intermodular de desenvolupament d\'aplicacions multiplataforma (segon curs)</li>',
+          '<li>Mòdul optatiu</li>',
+          '<li>Digitalització aplicada al sistema productiu (GS)</li>',
+          '<li>Sostenibilitat aplicada al sistema productiu</li>',
+          '<li>Formació en Centres de Treball (FCT)</li>',
+          '</ul>',
+          '</div>',
+        
+          '</div>',
+          '</div>'
+        ]
+    },
+    daw: {
+      titol: "Desenvolupament d'Aplicacions Web",
+      descripcio: "DAW"
+    },
+    fpb: {
+      titol: "Formació Professional Bàsica",
+      descripcio: "FPB"
+    },
+    fpqpi: {
+      titol: "Programa Formatiu de Qualificació Bàsica",
+      descripcio: "PFQB"
+    },
+    ceiabd: {
+      titol: "Curs d'Especialització de Inteligència Artificial i Big Data",
+      descripcio: "CEIABD"
+    }
+  };
