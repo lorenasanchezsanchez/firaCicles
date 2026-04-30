284f6949aa96f510933d9e49051002fd1657dd5a
Lorena Sánchez Sánchez <l.sanchezsanchez2@edu.gva.es>
2026-04-30 19:45:41 +0200

    Update ciclesData.js

--- js/ciclesData.js
+++ js/ciclesData.js
@@ -1,14 +1,12 @@
 export const ciclesData = {
-    smx: {
-      titol: "Sistemes Microinformàtics i Xarxes",
-      descripcio: "SMX",
-      alerta: "Procés d'admissió: del 23/05/26 al 02/06/26<br>Matrícula: del 24/06/26 al 02/07/26",
-      pla: [
+  smx: {
+    titol: "Sistemes Microinformàtics i Xarxes",
+    descripcio: "SMX",
+    alerta: "Procés d'admissió: del 23/05/26 al 02/06/26<br>Matrícula: del 24/06/26 al 02/07/26",
+    pla: [
       '<div class="panel-section">',
       '<h3>📚 Pla de formació de 2000 hores</h3>',
-
       '<div class="plan-formacio">',
-
       '<div class="curs">',
       '<h4>1r curs</h4>',
       '<ul>',
@@ -20,7 +18,6 @@ export const ciclesData = {
       '<li>Itinerari personal per a l’ocupabilitat I</li>',
       '</ul>',
       '</div>',
-
       '<div class="curs">',
       '<h4>2n curs</h4>',
       '<ul>',
@@ -36,67 +33,63 @@ export const ciclesData = {
       '<li>Formació en Centres de Treball (FCT)</li>',
       '</ul>',
       '</div>',
-
       '</div>',
       '</div>'
     ]
-    },
-    dam: {
-      titol: "Desenvolupament d'Aplicacions Multiplataforma",
-      descripcio: "DAM",
-      pla: [
-          '<div class="panel-section">',
-          '<h3>📚 Pla de formació de 2000 hores</h3>',
-        
-          '<div class="plan-formacio">',
-        
-          '<div class="curs">',
-          '<h4>1r curs</h4>',
-          '<ul>',
-          '<li>Anglés professional (GS)</li>',
-          '<li>Llenguatges de marques i sistemes de gestió d\'informació</li>',
-          '<li>Sistemes informàtics</li>',
-          '<li>Bases de dades</li>',
-          '<li>Programació</li>',
-          '<li>Entorns de desenvolupament</li>',
-          '<li>Itinerari personal per a l\'ocupabilitat I</li>',
-          '<li>Projecte intermodular de desenvolupament d\'aplicacions multiplataforma (primer curs)</li>',
-          '</ul>',
-          '</div>',
-        
-          '<div class="curs">',
-          '<h4>2n curs</h4>',
-          '<ul>',
-          '<li>Accés a dades</li>',
-          '<li>Desenvolupament d\'interfícies</li>',
-          '<li>Programació multimèdia i dispositius mòbils</li>',
-          '<li>Programació de serveis i processos</li>',
-          '<li>Sistemes de gestió empresarial</li>',
-          '<li>Itinerari personal per a l\'ocupabilitat II</li>',
-          '<li>Projecte intermodular de desenvolupament d\'aplicacions multiplataforma (segon curs)</li>',
-          '<li>Mòdul optatiu</li>',
-          '<li>Digitalització aplicada al sistema productiu (GS)</li>',
-          '<li>Sostenibilitat aplicada al sistema productiu</li>',
-          '<li>Formació en Centres de Treball (FCT)</li>',
-          '</ul>',
-          '</div>',
-        
-          '</div>',
-          '</div>'
-        ]
-    },
-    daw: {
-      titol: "Desenvolupament d'Aplicacions Web",
-      descripcio: "DAW",
-        pla: [
-  '<div class="panel-section">',
-  '<h3>📚 Pla de formació de 2000 hores</h3>',
-  '<div class="plan-formacio">',
+  }, // COMA AQUÍ
 
-  // 1r Curs
-  '<div class="curs">',
-    '<h4>1r curs</h4>',
-    '<ul>',
+  dam: {
+    titol: "Desenvolupament d'Aplicacions Multiplataforma",
+    descripcio: "DAM",
+    pla: [
+      '<div class="panel-section">',
+      '<h3>📚 Pla de formació de 2000 hores</h3>',
+      '<div class="plan-formacio">',
+      '<div class="curs">',
+      '<h4>1r curs</h4>',
+      '<ul>',
+      '<li>Anglés professional (GS)</li>',
+      '<li>Llenguatges de marques i sistemes de gestió d\'informació</li>',
+      '<li>Sistemes informàtics</li>',
+      '<li>Bases de dades</li>',
+      '<li>Programació</li>',
+      '<li>Entorns de desenvolupament</li>',
+      '<li>Itinerari personal per a l\'ocupabilitat I</li>',
+      '<li>Projecte intermodular de desenvolupament d\'aplicacions multiplataforma (primer curs)</li>',
+      '</ul>',
+      '</div>',
+      '<div class="curs">',
+      '<h4>2n curs</h4>',
+      '<ul>',
+      '<li>Accés a dades</li>',
+      '<li>Desenvolupament d\'interfícies</li>',
+      '<li>Programació multimèdia i dispositius mòbils</li>',
+      '<li>Programació de serveis i processos</li>',
+      '<li>Sistemes de gestió empresarial</li>',
+      '<li>Itinerari personal per a l\'ocupabilitat II</li>',
+      '<li>Projecte intermodular de desenvolupament d\'aplicacions multiplataforma (segon curs)</li>',
+      '<li>Mòdul optatiu</li>',
+      '<li>Digitalització aplicada al sistema productiu (GS)</li>',
+      '<li>Sostenibilitat aplicada al sistema productiu</li>',
+      '<li>Formació en Centres de Treball (FCT)</li>',
+      '</ul>',
+      '</div>',
+      '</div>',
+      '</div>'
+    ]
+  }, // COMA AQUÍ
+
+  daw: {
+    titol: "Desenvolupament d'Aplicacions Web",
+    descripcio: "DAW",
+    pla: [
+      '<div class="panel-section">',
+      '<h3>📚 Pla de formació de 2000 hores</h3>',
+      '<div class="plan-formacio">',
+      // Eliminado comentario, solo strings válidos
+      '<div class="curs">',
+      '<h4>1r curs</h4>',
+      '<ul>',
       '<li>Bases de dades</li>',
       '<li>Entorns de desenvolupament</li>',
       '<li>Formació i orientació laboral</li>',
@@ -106,13 +99,11 @@ export const ciclesData = {
       '<li>Anglès tècnic (GS)</li>',
       '<li>Itinerari personal per a l\'ocupabilitat I</li>',
       '<li>Projecte intermodular de desenvolupament d\'aplicacions web (I)</li>',
-    '</ul>',
-  '</div>',
-
-  // 2n Curs
-  '<div class="curs">',
-    '<h4>2n curs</h4>',
-    '<ul>',
+      '</ul>',
+      '</div>',
+      '<div class="curs">',
+      '<h4>2n curs</h4>',
+      '<ul>',
       '<li>Desenvolupament web en entorn client</li>',
       '<li>Desenvolupament web en entorn servidor</li>',
       '<li>Desplegament d\'aplicacions web</li>',
@@ -124,45 +115,44 @@ export const ciclesData = {
       '<li>Digitalització aplicada al sistema productiu (GS)</li>',
       '<li>Sostenibilitat aplicada al sistema productiu</li>',
       '<li>Formació en centres de treball (FCT)</li>',
-    '</ul>',
-  '</div>',
+      '</ul>',
+      '</div>',
+      '</div>',
+      '</div>'
+    ]
+  }, // COMA AQUÍ
 
-  '</div>',
-  '</div>'
-]
-    },
-    fpb: {
-      titol: "Formació Professional Bàsica",
-      descripcio: "FPB"
-    },
-    fpqpi: {
-      titol: "Programa Formatiu de Qualificació Bàsica",
-      descripcio: "PFQB"
-    },
-    ceiabd: {
-      titol: "Curs d'Especialització de Inteligència Artificial i Big Data",
-      descripcio: "CEIABD",
-      alerta: "PRESENCIAL",
-         pla: [
-          '<div class="panel-section">',
-          '<h3>📚 Pla de formació de 600 hores</h3>',
-        
-          '<div class="plan-formacio">',
-        
-          '<div class="curs">',
-          '<h4>MÒDULS</h4>',
-          '<ul>',
-          '<li>Models d'inteligència artificial</li>',
-          '<li>Sistemes d'aprenentatge automàtic</li>',
-          '<li>Programació d'intel·ligència artificial</li>',
-          '<li>Sistemes en Big Data</li>',
-          '<li>Big Data aplicat</li>',
-          '<li>Formació en Empresa Obligatòria</li>',
-          '</ul>',
-          '</div>',             
-        
-          '</div>',
-          '</div>'
-        ]
-    }
-  };
+  fpb: {
+    titol: "Formació Professional Bàsica",
+    descripcio: "FPB"
+  },
+
+  fpqpi: {
+    titol: "Programa Formatiu de Qualificació Bàsica",
+    descripcio: "PFQB"
+  },
+
+  ceiabd: {
+    titol: "Curs d'Especialització de Inteligència Artificial i Big Data",
+    descripcio: "CEIABD",
+    alerta: "PRESENCIAL",
+    pla: [
+      '<div class="panel-section">',
+      '<h3>📚 Pla de formació de 600 hores</h3>',
+      '<div class="plan-formacio">',
+      '<div class="curs">',
+      '<h4>MÒDULS</h4>',
+      '<ul>',
+      '<li>Models d\'inteligència artificial</li>',
+      '<li>Sistemes d\'aprenentatge automàtic</li>',
+      '<li>Programació d\'intel·ligència artificial</li>',
+      '<li>Sistemes en Big Data</li>',
+      '<li>Big Data aplicat</li>',
+      '<li>Formació en Empresa Obligatòria</li>',
+      '</ul>',
+      '</div>',             
+      '</div>',
+      '</div>'
+    ]
+  }
+};
